c00dd62967557c12d81f4bf467adf7bdc7480e2f
Kirill Volosevich <keriaz@mail.ru>
2022-05-02 13:28:41 +0300

    fix: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function createDreamTeam(members) {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
     if (!Array.isArray(members)) {
         return false
